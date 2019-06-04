953e79b0d6e02108e75b9c4d962a4812e15389e7
Ricky Davis <ricky.c.davis.9@gmail.com>
2019-06-04 14:34:48 -0500

    Finished exercise section 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -14,3 +14,19 @@ where birth_date like "%-12-25";
 
 select * from employees
 where last_name like "%q%";
+
+select * from employees
+where (first_name = "Irena" or first_name = "Vidya" or  first_name = "Maya") and gender="M";
+
+select * from employees
+where last_name like "E%" or last_name like "%e";
+
+select * from employees
+where last_name like "E%E";
+
+
+select * from employees
+where birth_date like "%-12-25" and hire_date like "199%";
+
+select * from employees
+where last_name like "%q%" and last_name not like "%qu%";
